e73a422f99758f0af2ef241314eee27876a66eaa
Bijan Thing <bijantheeng@gmail.com>
2026-02-17 17:31:58 +0545

    removed postgresql for now

--- core/settings.py
+++ core/settings.py
@@ -94,10 +94,6 @@ DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE'),
         'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
     }
 }
 
